95ccd394fd4fb6fa63c12d6da5ac0e269d6e8b75
tylermatrix <tylermaatrix@gmail.com>
2021-10-06 19:39:44 -0400

    big update
    added folder structure for components and pages. added images to individual menu components. Used a class component? Passing a lot  of props through components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepages.styles.scss";
+import Directory from "../../components/directory/directory.component";
+import MenuItem from "../../components/menu-item/menu-item.component";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
