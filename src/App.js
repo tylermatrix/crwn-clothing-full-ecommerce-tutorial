b4cd5b05da175951acf223f676be620e71d22178
tylermatrix <tylermaatrix@gmail.com>
2021-11-03 17:51:49 -0400

    created signin page and component with handlers and state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Homepage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
