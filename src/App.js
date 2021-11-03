82339ffdd7ffd82fe5b2491ee9504d03a9b610f4
tylermatrix <tylermaatrix@gmail.com>
2021-11-03 16:04:00 -0400

    created the shop component and collection preview
    Important to know I am creating this using a class component which will be refactored into a function component for hooks later.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" exact component={Homepage}></Route>
-        <Route path="/shop/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
